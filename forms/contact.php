30e1149496b54736482da9e8b5ecf1f5f25f8bd4
Tasnimul Hasan <63117803+nh-404@users.noreply.github.com>
2024-05-11 16:34:58 +0600

    Update contact.php

--- forms/contact.php
+++ forms/contact.php
@@ -1,36 +1,34 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Get the form fields and remove whitespace
-    $name = strip_tags(trim($_POST["name"]));
-    $name = str_replace(array("\r","\n"),array(" "," "),$name);
-    $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
-    $subject = trim($_POST["subject"]);
-    $message = trim($_POST["message"]);
 
-    // Set the recipient email address
-    $to = "nh.404@outlook.com";
+  $receiving_email_address = 'nh.404@outlook.com';
 
-    // Set the email subject
-    $email_subject = "New contact form submission: $subject";
+  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
+    include( $php_email_form );
+  } else {
+    die( 'Unable to load the "PHP Email Form" Library!');
+  }
 
-    // Build the email content
-    $email_content = "Name: $name\n";
-    $email_content .= "Email: $email\n\n";
-    $email_content .= "Message:\n$message\n";
+  $contact = new PHP_Email_Form;
+  $contact->ajax = true;
+  
+  $contact->to = $receiving_email_address;
+  $contact->from_name = $_POST['name'];
+  $contact->from_email = $_POST['email'];
+  $contact->subject = $_POST['subject'];
 
-    // Build the email headers
-    $email_headers = "From: $name <$email>";
+  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
+  /*
+  $contact->smtp = array(
+    'host' => 'example.com',
+    'username' => 'example',
+    'password' => 'pass',
+    'port' => '587'
+  );
+  */
 
-    // Send the email
-    if (mail($to, $email_subject, $email_content, $email_headers)) {
-        http_response_code(200);
-        echo "Your message has been sent. Thank you!";
-    } else {
-        http_response_code(500);
-        echo "Oops! Something went wrong and we couldn't send your message.";
-    }
-} else {
-    http_response_code(403);
-    echo "There was a problem with your submission, please try again.";
-}
+  $contact->add_message( $_POST['name'], 'From');
+  $contact->add_message( $_POST['email'], 'Email');
+  $contact->add_message( $_POST['message'], 'Message', 10);
+
+  echo $contact->send();
 ?>
